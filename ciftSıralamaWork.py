3064531547a767c957f9b81792de3eeb5c5c9860
Büşra Altuntaş <99415208+busraaltuuntas@users.noreply.github.com>
2023-04-07 12:49:10 +0300

    pair7

--- ciftSıralamaWork.py
+++ ciftSıralamaWork.py
@@ -1,10 +1,16 @@
 def ciftsayilar():
     list2 = []
     x = int(input ("Lütfen bir sayı giriniz: "))
+    y= int(input("alt limit belirleryiniz"))
     if x>0:
-        for i in range(0,x,2):
-            list2.append(i)
-        print(list2)
+        if y%2==0:
+            for i in range(y,x,2):
+                list2.append(i)
+            print(list2)
+        else:
+            for i in range(y+1,x,2):
+                list2.append(i)
+            print(list2)
     else:
         print("Lütfen 0'dan büyük sayı giriniz.")
         ciftsayilar()
