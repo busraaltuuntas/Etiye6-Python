0ff4f905049fc13e0a1d3c976e60523d5516e8a7
Büşra Altuntaş <99415208+busraaltuuntas@users.noreply.github.com>
2023-04-07 15:35:58 +0300

    pair7

---
+++ mukemmelSayi.py
@@ -0,0 +1,10 @@
+sayi = int(input("Sayı giriniz: "))
+toplam = 0
+
+for i in range (1, sayi):
+    if ((sayi % i) == 0):
+        toplam =toplam +i
+if (sayi == toplam):
+    print("Mükemmel sayıdır")
+else:
+    print("Mükemmel sayı değildir")
