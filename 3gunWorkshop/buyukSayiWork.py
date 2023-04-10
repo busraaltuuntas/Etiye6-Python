f812789fcad71af939faf4f35d4a2be78b3b25e0
Büşra Altuntaş <99415208+busraaltuuntas@users.noreply.github.com>
2023-04-10 09:39:47 +0300

    pair7

---
+++ 3gunWorkshop/buyukSayiWork.py
@@ -0,0 +1,13 @@
+def buyukSayi():
+    list = []
+    for i in range(10):
+        sayi = int(input(f'{i+1}.sayıyı giriniz :') )
+        list.append(sayi)
+
+    print("En büyük sayı :" ,max(list))
+buyukSayi()
+
+
+
+
+
---
+++ 3gunWorkshop/ciftSıralamaWork.py
@@ -0,0 +1,18 @@
+def ciftsayilar():
+    list2 = []
+    x = int(input ("Lütfen bir sayı giriniz: "))
+    y= int(input("alt limit belirleryiniz"))
+    if x>0:
+        #limit eklemek için
+        if y%2==0:
+            for i in range(y,x,2):
+                list2.append(i)
+            print(list2)
+        else:
+            for i in range(y+1,x,2):
+                list2.append(i)
+            print(list2)
+    else:
+        print("Lütfen 0'dan büyük sayı giriniz.")
+        ciftsayilar()
+ciftsayilar()
